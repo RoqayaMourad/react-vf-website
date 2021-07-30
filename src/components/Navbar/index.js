397b468d9d9b435e08aea73d350694bb7f411326
roqaya299 <elephantsong29@example.com>
2021-07-30 05:38:15 +0200

    navbar style

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,42 @@
 import React from 'react'
+import {FaBars} from 'react-icons/fa'
+import {Nav , NavbarContainer , NavLogo , MobileIcon , NavMenu , NavItem , NavLinks , NavBtnLink , NavBtn} from './NavbarElement'
 
 const Navbar = () => {
     return (
         <>
         
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to="/">
+                dolla
+                </NavLogo>
+                <MobileIcon>
+                    <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to='about'> About </NavLinks>       
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to='Discover'> Discover </NavLinks>       
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to='Services'> Services </NavLinks>       
+                    </NavItem>
+                    <NavItem>
+                        <NavLinks to='Sign Up'> Sign Up </NavLinks>       
+                    </NavItem>
+                </NavMenu>
 
+                <NavBtn>
+                    <NavBtnLink to="/signin">
+                    Sign in 
+                    </NavBtnLink>
+                </NavBtn>
+            </NavbarContainer>
+        </Nav>
         </>
     )
 }
-
 export default Navbar
